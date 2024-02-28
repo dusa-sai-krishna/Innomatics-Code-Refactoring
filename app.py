2dd576db76cd2970d3878be1277fc8fa95c0c1e1
Sai Krishna <dsai9441695421@gmail.com>
2024-02-28 10:36:21 +0530

    third commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def add_note():
     note = request.form.get("note")
     count=len(notes)
     print(notes,note,count)
-    if (count==0 and note not in [None,'']) or (count>1 and notes[-1]!=note and note not in [None,'']):
+    if (count==0 and note not in [None,'']) or (count>0 and notes[-1]!=note and note not in [None,'']):
         notes.append(note)
         count+=1
     return render_template('home.html',notes=notes,count=len(notes))
