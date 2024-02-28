36c249cdd30c26a917bdd8f248bdcf8ea1303c1d
Sai Krishna <dsai9441695421@gmail.com>
2024-02-28 09:45:58 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+notes = []
+@app.route('/')
+def load_home():
+    return render_template('home.html',notes=notes,count=len(notes))
+
+@app.route('/addnote',methods=['POST'])
+def add_note():
+    note = request.form.get("note")
+    count=len(notes)
+    print(notes,note,count)
+    if (count==0 and note not in [None,'']) or (count==1 and notes[-1]!=note and note not in [None,'']):
+        notes.append(note)
+    return render_template('home.html',notes=notes,count=len(notes))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
